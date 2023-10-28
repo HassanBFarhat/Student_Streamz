36d05521d844312b7de9f44bd71853cd5c9067e5
HassanBFarhat <farhathassan@rocketmail.com>
2023-10-28 16:56:05 -0700

    4th Commit: Added NavBar that links various pages

--- student_streamz/src/App.js
+++ student_streamz/src/App.js
@@ -7,17 +7,19 @@ import NotFoundPage from './webpages/NotFoundPage';
 import ShowSelectionPage from './webpages/ShowSelectionPage';
 import VideoInformationPage from './webpages/VideoInformationPage';
 import VideoPlayerPage from './webpages/VideoPlayerPage';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>TEST</h1>
+      {/* <h1>TEST</h1> */}
+      <NavBar></NavBar>
       <Routes>
-        <Route exact path="./webpages/HomePage.js" element={<HomePage></HomePage>}></Route>
-        <Route path='./webpages/NotFoundPage.js' element={<NotFoundPage></NotFoundPage>}></Route>
-        <Route path='./webpages/ShowSelectionPage.js' element={<ShowSelectionPage></ShowSelectionPage>}></Route>
-        <Route path='./webpages/VideoInformationPage.js' element={<VideoInformationPage></VideoInformationPage>}></Route>
-        <Route path='./webpages/VideoPlayer.js' element={<VideoPlayerPage></VideoPlayerPage>}></Route>
+        <Route exact path="../webpages/HomePage.js" element={<HomePage></HomePage>}></Route>
+        <Route path='../webpages/NotFoundPage.js' element={<NotFoundPage></NotFoundPage>}></Route>
+        <Route path='../webpages/ShowSelectionPage.js' element={<ShowSelectionPage></ShowSelectionPage>}></Route>
+        <Route path='../webpages/VideoInformationPage.js' element={<VideoInformationPage></VideoInformationPage>}></Route>
+        <Route path='../webpages/VideoPlayerPage.js' element={<VideoPlayerPage></VideoPlayerPage>}></Route>
       </Routes>
     </div>
   );
---
+++ student_streamz/src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+
+    function changed() {
+        console.log("Button Clicked");
+    }
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="../webpages/HomePage.js" onClick={changed}>HomePage</Link>
+                </li>
+                <li>
+                    <Link to="../webpages/NotFoundPage.js" onClick={changed}>NotFoundPage</Link>
+                </li>
+                <li>
+                    <Link to="../webpages/VideoInformationPage.js" onClick={changed}>VideoInformationPage</Link>
+                </li>
+                <li>
+                    <Link to="../webpages/ShowSelectionPage.js" onClick={changed}>ShowSelectionPage</Link>
+                </li>
+                <li>
+                    <Link to="../webpages/VideoPlayerPage.js" onClick={changed}>VideoPlayerPage</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
