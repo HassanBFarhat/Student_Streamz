b2387d83c6a8bf292911c7540101ff0d7113f183
Avi <abavisetty3@gmail.com>
2023-11-18 09:52:16 -0800

    add a variety of tv shows to the front kind of as a recommended like type of thing for new viewers of the site. can enter season number and episode number to pull up different episodes and seasons. Updated the index.css

--- student_streamz/src/webpages/HomePage.js
+++ student_streamz/src/webpages/HomePage.js
@@ -1,51 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function VideoBox({ showTitle, imdbID }) {
+    const [selectedSeason, setSelectedSeason] = useState(1);
+    const [selectedEpisode, setSelectedEpisode] = useState(1);
+
+    const handleSeasonChange = (event) => {
+        setSelectedSeason(parseInt(event.target.value, 10));
+    };
+
+    const handleEpisodeChange = (event) => {
+        setSelectedEpisode(parseInt(event.target.value, 10));
+    };
+
+    const handleWatchClick = () => {
+        // Handle the watch button click, e.g., update the iframe src
+        const videoUrl = `https://vidsrc.xyz/embed/tv?imdb=${imdbID}&season=${selectedSeason}&episode=${selectedEpisode}`;
+        document.getElementById(`${imdbID}-videoFrame`).src = videoUrl;
+    };
+
+    return (
+        <div className="video-box">
+            <h2>{showTitle}</h2>
+
+            <div className="video-container">
+                <iframe
+                    id={`${imdbID}-videoFrame`}
+                    width="280" // Adjust the width as needed
+                    height="160" // Adjust the height as needed
+                    src={`https://vidsrc.xyz/embed/tv?imdb=${imdbID}&season=${selectedSeason}&episode=${selectedEpisode}`}
+                    allowFullScreen
+                ></iframe>
+
+                <div className="input-boxes">
+                    <label htmlFor={`${imdbID}-season`}>Enter Season:</label>
+                    <input
+                        type="number"
+                        id={`${imdbID}-season`}
+                        value={selectedSeason}
+                        onChange={handleSeasonChange}
+                        min={1}
+                    />
+
+                    <label htmlFor={`${imdbID}-episode`}>Enter Episode:</label>
+                    <input
+                        type="number"
+                        id={`${imdbID}-episode`}
+                        value={selectedEpisode}
+                        onChange={handleEpisodeChange}
+                        min={1}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
 
 function HomePage() {
+    // Replace these with actual show data
+    const shows = [
+        { title: 'The Wire', imdbID: 'tt0306414' },
+        { title: 'The Sopranos', imdbID: 'tt0141842' },
+        { title: 'The Fall', imdbID: 'tt2294189' },
+        { title: 'Breaking Bad', imdbID: 'tt0903747' },
+        { title: 'House', imdbID: 'tt0412142' },
+        { title: 'BoJack Horseman', imdbID: 'tt3398228' },
+        { title: 'Oz', imdbID: 'tt0118421' },
+        { title: 'Boardwalk Empire', imdbID: 'tt0979432' },
+        { title: 'Chappelle\'s Show', imdbID: 'tt0353049' },
+        { title: 'Freaks and Geeks', imdbID: 'tt0193676' },
+        { title: 'The X-Files', imdbID: 'tt0106179' },
+        { title: 'Spawn', imdbID: 'tt0118475' },
+        { title: 'The Boondocks', imdbID: 'tt0373732' },
+        { title: 'Twin Peaks', imdbID: 'tt0098936' },
+        { title: 'Silicon Valley', imdbID: 'tt2575988' },
+        { title: 'Mindhunter', imdbID: 'tt5290382' },
+        { title: 'Family Guy', imdbID: 'tt0182576' },
+        { title: 'South Park', imdbID: 'tt0121955' },
+        { title: 'Cops', imdbID: 'tt0096563' },
+        { title: 'Deadwood', imdbID: 'tt0348914' },
+        { title: 'The Jamie Foxx Show', imdbID: 'tt0115221' },
+        { title: 'Malcolm in the Middle', imdbID: 'tt0212671' },
+    ];
+
     return (
         <>
             <h1>HomePage</h1>
-            <h1>HomePage</h1>
-            <h1>HomePage</h1>
-            <button>Hello</button>
-
-            <div className="video-container">
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
-
+            <div className="video-grid">
+                {shows.map((show) => (
+                    <VideoBox key={show.imdbID} showTitle={show.title} imdbID={show.imdbID} />
+                ))}
             </div>
         </>
     );
