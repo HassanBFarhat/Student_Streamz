ba331ecabf0bc4edc594fc6792d4825f924944ed
Avi <abavisetty3@gmail.com>
2023-11-17 22:34:33 -0800

    Temporary change to the HomePage.js to show gallery of videos appearing on screen

--- student_streamz/src/webpages/HomePage.js
+++ student_streamz/src/webpages/HomePage.js
@@ -1,3 +1,4 @@
+import React from 'react';
 
 function HomePage() {
     return (
@@ -6,6 +7,46 @@ function HomePage() {
             <h1>HomePage</h1>
             <h1>HomePage</h1>
             <button>Hello</button>
+
+            <div className="video-container">
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://vidsrc.xyz/embed/tv?imdb=tt0944947" allowFullScreen></iframe>
+
+            </div>
         </>
     );
 }
