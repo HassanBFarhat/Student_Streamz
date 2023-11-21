75f3084c8e6ed292e53af55da7b3301e6beb8d8a
HassanBFarhat <farhathassan@rocketmail.com>
2023-11-21 00:08:34 -0800

    16th: Display unstyled data for movies/shows

--- student_streamz/src/webpages/MovieInformationPage.js
+++ student_streamz/src/webpages/MovieInformationPage.js
@@ -1,8 +1,45 @@
+import React, { useEffect, useState } from 'react';
 
 function MovieInformationPage() {
+
+    const movieId = decodeURIComponent(window.location.href.split('/').pop());
+
+    const apiKey = `e5aab5ab325195040b8c8598f9ba0a51`;
+    const movieDetailsUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
+    const imgUrl = `https://image.tmdb.org/t/p/original`;
+
+    const [apiMovieDetails, setApiMovieDetails] = useState([]);
+
+    const fetchMovieDetailsFromApi = async () => {
+        const response = await fetch(movieDetailsUrl);
+        const data = await response.json();
+        setApiMovieDetails(data);
+    };
+
+    useEffect(() => {
+        fetchMovieDetailsFromApi();
+    }, []);
+
+    if (apiMovieDetails === undefined) {
+        return null;
+    }
+
+    // ****** DEBUGGER ******
+    console.log(apiMovieDetails);
+    // ****** DEBUGGER ******    
+
     return (
         <>
         <h1>MovieInformationPage</h1>
+        <h1>Title: {apiMovieDetails.title}</h1>
+        <h1>TagLine: {apiMovieDetails.tagline}</h1>
+        <h1>Overview: {apiMovieDetails.overview}</h1>
+        <h1>Runtime: {apiMovieDetails.runtime}</h1>
+        <h1>Realse Date: {apiMovieDetails.release_date}</h1>
+        <h1>Status: {apiMovieDetails.status}</h1>
+        <h1>Budget: {apiMovieDetails.budget}</h1>
+        <h1>Revenue: {apiMovieDetails.revenue}</h1>
+        <h1>Poster: <img src={imgUrl + apiMovieDetails.poster_path}/></h1>
         </>
     );
 }
--- student_streamz/src/webpages/ShowInformationPage.js
+++ student_streamz/src/webpages/ShowInformationPage.js
@@ -1,8 +1,46 @@
+import React, { useEffect, useState } from 'react';
 
 function ShowInformationPage() {
+
+    const tvId = decodeURIComponent(window.location.href.split('/').pop());
+
+    const apiKey = `e5aab5ab325195040b8c8598f9ba0a51`;
+    const showDetailsUrl  = `https://api.themoviedb.org/3/tv/${tvId}?api_key=${apiKey}`;
+    const imgUrl = `https://image.tmdb.org/t/p/original`;
+
+    const [apiShowDetails, setApiShowDetails] = useState([]);
+
+    const fetchShowDetailsFromApi = async () => {
+        const response = await fetch(showDetailsUrl);
+        const data = await response.json();
+        setApiShowDetails(data);
+    };
+
+    useEffect(() => {
+        fetchShowDetailsFromApi();
+    }, []);
+
+    if (apiShowDetails === undefined) {
+        return null;
+    }
+
+    // ****** DEBUGGER ******
+    console.log(apiShowDetails);
+    // ****** DEBUGGER ******    
+
+
     return (
         <>
         <h1>ShowInformationPage</h1>
+        <h1>Title: {apiShowDetails.name}</h1>
+        <h1>Original Title: {apiShowDetails.original_name}</h1>
+        <h1>Tagline: {apiShowDetails.tagline}</h1>
+        <h1>Overview: {apiShowDetails.overview}</h1>
+        <h1>Air Date: {apiShowDetails.first_air_date}</h1>
+        <h1>Status: {apiShowDetails.status}</h1>
+        <h1># of Seasons: {apiShowDetails.number_of_seasons}</h1>
+        <h1># of Episodes: {apiShowDetails.number_of_episodes}</h1>
+        <h1>Poster: <img src={imgUrl + apiShowDetails.poster_path}/></h1>
         </>
     );
 }
