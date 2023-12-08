f87648799d5a9804bbd9b1e287512ff46f6b61c2
HassanBFarhat <farhathassan@rocketmail.com>
2023-12-07 18:46:35 -0800

    25th: Added style sheet to both Login/SignUp pages

--- student_streamz/src/webpages/LoginPage.js
+++ student_streamz/src/webpages/LoginPage.js
@@ -1,17 +1,24 @@
+import "../Login&SignUpPage.css";
 
 function LoginPage() {
     return (
         <>
-            <h1>Login Page</h1>
-            <label>Username:</label>
-            <input type="Text"></input>
             <br />
-            <label>Password:</label>
-            <input type="Password"></input>
-            <br />
-            <button>Sign In</button>
-            <br />
-            <p><a href="#/sign_up">or Click Here to Sign Up</a></p>
+            <h2>Please Enter Username and Password if you already have an Account to Login</h2>
+            <div className="login-container">
+                <label id="username">Username:</label>
+                <input id="usernameInput" type="Text"></input>
+                <br />
+                <label id="password">Password:</label>
+                <input id="passwordInput" type="Password"></input>
+                <br />
+                <button>Sign In</button>
+                <br />
+                <br />
+                <br />
+                <br />
+                <p><a href="#/sign_up">or Click Here to Sign Up</a></p>
+            </div>
         </>
     );
 }
--- student_streamz/src/webpages/SignUpPage.js
+++ student_streamz/src/webpages/SignUpPage.js
@@ -1,36 +1,43 @@
+import "../Login&SignUpPage.css";
 
 function SignUpPage() {
     return (
         <>
-            <h1>Sign Up Page</h1>
-            <label>First Name:</label>
-            <input type="Text"></input>
             <br />
-            <label>Last Name:</label>
-            <input type="Text"></input>
-            <br />
-            <label>Birthday:</label>
-            <input type="Date"></input>
-            <br />
-            <label>Gender:</label>
-            <select>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-            </select>
-            <br />
-            <label>Username:</label>
-            <input type="Text"></input>
-            <br />
-            <label>Password:</label>
-            <input type="Password"></input>
-            <br />
-            <label>Re-Enter Password:</label>
-            <input type="Password"></input>
-            <br />
-            <button>Create Account</button>
-            <br />
-            <p><a href="#/login">If you already have an account, please Click Here to Login</a></p>
+            <h2>Please Enter All the Available Information in Order to Create an Account </h2>
+            <div className="sign-up-container">
+                <label id="first-name">First Name:</label>
+                <input id="first-name-input"type="Text"></input>
+                <br />
+                <label id="last-name">Last Name:</label>
+                <input id="last-name-input" type="Text"></input>
+                <br />
+                <label id="birthday">Birthday:</label>
+                <input id="birthday-input" type="Date"></input>
+                <br />
+                <label id="gender">Gender:</label>
+                <select id="gender-input">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                </select>
+                <br />
+                <label id="username2">Username:</label>
+                <input id="username2-input" type="Text"></input>
+                <br />
+                <label id="password2">Password:</label>
+                <input id="password2-input" type="Password"></input>
+                <br />
+                <label id="re-entered-password">Re-Type Password:</label>
+                <input id="re-entered-password-input"type="Password"></input>
+                <br />
+                <button>Create Account</button>
+                <br />
+                <br />
+                <br />
+                <br />
+                <p><a href="#/login">If you already have an account, please Click Here to Login</a></p>
+            </div>
         </>
     );
 }
